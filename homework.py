a652d7c77da4b4290dfd8054c64bb144e50ed3ff
VankoID <dudarev.id@gmail.com>
2022-02-16 21:54:58 +0400

    All changes are done

--- homework.py
+++ homework.py
@@ -1,16 +1,15 @@
+from dataclasses import dataclass
 from typing import List, Dict
 
 
+@dataclass
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    def __init__(self, training_type: str,
-                 duration: float, distance: float,
-                 speed: float, calories: float) -> None:
-        self.training_type = training_type
-        self.duration = duration
-        self.distance = distance
-        self.speed = speed
-        self.calories = calories
+    training_type: str
+    duration: float
+    distance: float
+    speed: float
+    calories: float
 
     def get_message(self) -> str:
         message_to_print: str = (f'Тип тренировки: {self.training_type}; '
